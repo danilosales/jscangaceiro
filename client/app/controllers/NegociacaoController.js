d3ad512c7f418f5c1fe7834d6367b2cbc14ebc4b
Danilo Sales <danilosales.info@gmail.com>
2019-03-16 08:57:42 -0300

    Alterações capitulo 7

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,7 +1,7 @@
 class NegociacaoController {
 
     constructor() {
-        let $ = document.querySelector.bind(document);
+        const $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
@@ -9,12 +9,18 @@ class NegociacaoController {
         this._negociacoesView = new NegociacoesView('#negociacoes');
 
         this._negociacoesView.update(this._negociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView('#mensagemView');
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
         event.preventDefault();
         this._negociacoes.adiciona(this._criarNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
---
+++ client/app/ui/views/View.js
@@ -0,0 +1,14 @@
+class View {
+
+    constructor(elemento) {
+        this._elemento = document.querySelector(elemento);
+    }
+
+    update(model) {
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model) {
+        throw new Error('Você precisa implementar o método template');
+    }
+}
