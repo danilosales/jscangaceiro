82d7a5f5ce06b13d368e1aeccfbe6984bad565f7
Danilo Sales <danilosales.info@gmail.com>
2019-03-16 07:23:39 -0300

    Alterações capitulo 5

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,23 +5,27 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
+        this._negociacoes.adiciona(this._criarNegociacao());
+        this._limpaFormulario();
+    }
 
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
 
-        let negociacao = new Negociacao(
-            data,
+    _criarNegociacao() {
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.valeu)
         );
-    
-        console.log(negociacao);
     }
 }
